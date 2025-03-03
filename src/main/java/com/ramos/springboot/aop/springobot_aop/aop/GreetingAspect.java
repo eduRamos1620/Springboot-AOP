e90c8c486d94abd9f9d771da4f6c3da321ee7993
eduRamos1620 <eduardoramosrosales16@gmail.com>
2025-03-03 10:01:07 -0600

    Se usa Around

--- src/main/java/com/ramos/springboot/aop/springobot_aop/aop/GreetingAspect.java
+++ src/main/java/com/ramos/springboot/aop/springobot_aop/aop/GreetingAspect.java
@@ -3,9 +3,11 @@ package com.ramos.springboot.aop.springobot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -24,22 +26,39 @@ public class GreetingAspect {
         logger.info("Antes: " + method + " con los argumentos " + args);
     }
 
-    @After("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.sayHello(..))")
+    @After("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.*(..))")
     public void loggerAfter(JoinPoint joinPoint){
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues: " + method + " con los argumentos " + args);
     }
-    @AfterReturning("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.sayHello(..))")
+    @AfterReturning("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.*(..))")
     public void loggerAfterReturning(JoinPoint joinPoint){
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues de retornar: " + method + " con los argumentos " + args);
     }
-    @AfterThrowing("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.sayHelloError(..))")
+    @AfterThrowing("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.*(..))")
     public void loggerAfterThrowing(JoinPoint joinPoint){
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues de lanzar la excepcion: " + method + " con los argumentos " + args);
+    }
+
+    @Around("execution(* com.ramos.springboot.aop.springobot_aop.services.GreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        Object result = null;
+        try {
+            logger.info("El metodo " + method + "() con los parametros " + args);
+            result = joinPoint.proceed();
+            logger.info("El metodo " + method + "() retorna el resultado: " + result);
+            return null;
+        } catch (Throwable e) {
+            logger.error("Error en la llamda del metodo " + method);
+            throw e;
+        }
+
     }
 }
